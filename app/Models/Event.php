784dafb6a0c788932d31ab279691c62bccd70f54
Jayant Ochani <jochani@gmail.com>
2022-06-09 19:17:20 +1200

    second commit for showing all events and workshops

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+	protected $table = 'events';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+	/**
+     * Get the workshops for the events.
+     */
+    public function workshops()
+    {
+        //return $this->hasMany(Workshop::class);
+		return $this->hasMany(Workshop::class);
+    }
+	
+
 }
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -9,5 +9,18 @@ use Illuminate\Support\Facades\Date;
 
 class Workshop extends Model
 {
+	protected $table = 'workshops';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+	public function event()
+    {
+        return $this->belongsTo(Event::class, 'id', 'event_id');
+    }
 
 }
