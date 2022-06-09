ced9b7dc5ee52814cdb63d4119c518560fc44c1b
Jayant Ochani <jochani@gmail.com>
2022-06-09 19:31:26 +1200

    fourth commit for showing all menus

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,21 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+	protected $fillable = ['parent_id', 'name'];
+
+	public function parent()
+	{
+		return $this->hasOne('App\Models\MenuItem', 'id', 'parent_id')->orderBy('id');
+	}
+
+	public function children()
+	{
+		return $this->hasMany('App\Models\MenuItem', 'parent_id', 'id')->orderBy('id');
+	}
+
+	public static function menutree()
+	{
+		return static::with(implode('.', array_fill(0, 100, 'children')))->where('parent_id', '=', Null)->orderBy('id')->get();
+	}
+
 }
